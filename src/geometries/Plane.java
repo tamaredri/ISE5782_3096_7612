11e2d7e4b6c3209e49017f4c5a226c7b38dda0c5
Yael <yshiloni@g.jct.ac.il>
2022-02-24 14:18:53 +0200

    - implement plane and triangle left to do: * check the input in function parameters (null) * vector zero check in constructors and in other different methods

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -1,4 +1,30 @@
-package geometries;
+package geometries.*;
+package primitives.*;
 
 public class Plane {
+
+    private Point q0;
+    private Vector normal;
+
+    public Plane(Point p1, Point p2, Point p3) {
+        Vector v1 = new vector (p1.subtract(p2));
+        Vector v2 = new Vector (p2.subtract(p3));
+        this.normal = v1.crossProduct(v2).normalize;
+        this.q0 = p1;
+    }
+
+    public Plane(Point p, Vector normal) {
+        this.q0 = p;
+        this.normal = normal;
+    }
+
+    public Point getQ0() {
+        return q0;
+    }
+
+    public Vector getNormal() {
+        return normal;
+    }
+
+
 }
