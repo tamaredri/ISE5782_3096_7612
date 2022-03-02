21b21e32be0e0b1a479f1d74cbd47faa53389b7c
tamar edri <tedri@g.jct.ac.il>
2022-03-02 17:04:49 +0200

    - generate testers for Vector, Polygon and Cylinder left to do: * JUnit = test unit * implement of getNormal

---
+++ unitTests/geometries/CylinderTest.java
@@ -0,0 +1,12 @@
+package geometries;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CylinderTest {
+
+    @Test
+    void testGetNormal() {
+    }
+}
