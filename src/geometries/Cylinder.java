931df8b38692710a856c567b788f16faca065599
Yael <yshiloni@g.jct.ac.il>
2022-05-29 11:28:24 +0300

    - implement findIntersections for Cylinder left to do: * create scene for mini project * move camera bonus * fix polygon bug * findIntersections for the bases of cylinder * glossy/diffusive surface * anti aliasing * Improving efficiency

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -1,7 +1,11 @@
 package geometries;
 
 import primitives.*;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static primitives.Util.alignZero;
 
 public class Cylinder extends Tube{
 
@@ -48,7 +52,49 @@ public class Cylinder extends Tube{
     //region findGeoIntersectionsHelper
     @Override
     public List<GeoPoint> findGeoIntersectionsHelper(Ray ray, double masDistance) {
-        return null;
+        Point o = ray.getP0();
+        Vector d = ray.getDir();
+        Vector local_z = axisRay.getDir().normalize();
+        Vector local_x = d.crossProduct(local_z).normalize();
+        Vector local_y = local_z.crossProduct(local_x).normalize();
+        Vector w = o.subtract(axisRay.getP0());
+        Vector o_local = new Vector(w.dotProduct(local_x), w.dotProduct(local_y), w.dotProduct(local_z));
+        Vector d_local = new Vector(d.dotProduct(local_x), d.dotProduct(local_y), d.dotProduct(local_z));
+        double a = d_local.getY() * d_local.getY();
+        double b = 2 * d_local.getY() * o_local.getY();
+        double c = o_local.getY() * o_local.getY() + o_local.getX() * o_local.getX() - radius * radius;
+        if (a == 0) {
+            return null;
+        }
+        double e = b * b - 4 * a * c;
+        if (e < 0) {
+            return null;
+        }
+
+        e = Math.sqrt(e);
+
+        List<GeoPoint> pointList = new ArrayList<>();
+
+        double t1 = (-b - e) / (2 * a);
+        double t2 = (-b + e) / (2 * a);
+        Point p2 = axisRay.getP0().add(axisRay.getDir().scale(height));
+        if (alignZero(t1) > 0d) {
+            Point i = ray.getPoint(t1);
+            double f = i.subtract(axisRay.getP0()).dotProduct(local_z);
+            if (f > 0 && f < p2.subtract(axisRay.getP0()).length()) {
+                pointList.add(new GeoPoint(this , i));
+            }
+        }
+        if (alignZero(t2) > 0d) {
+            Point i = ray.getPoint(t2);
+            double f = i.subtract(axisRay.getP0()).dotProduct(local_z);
+            if (f > 0 && f < p2.subtract(axisRay.getP0()).length()) {
+                pointList.add(new GeoPoint(this , i));
+            }
+        }
+        if(pointList.size() == 0)
+            return null;
+        return pointList;
     }
     //endregion
 }
